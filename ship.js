6277102d86fb5873b2b2bfa4e2c9a0f5f88ded17
David Zhu <davidyzhu2@gmail.com>
2025-01-06 11:31:29 -0800

    Add ship class and tests

--- ship.js
+++ ship.js
@@ -1,1 +1,22 @@
-export class Ship {}
+export class Ship {
+  length;
+  hits;
+  sunk;
+
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+    this.hits++;
+    if (this.hits == this.length) {
+      this.sunk = true;
+    }
+  }
+
+  isSunk() {
+    return this.sunk;
+  }
+}
--- ship.spec.js
+++ ship.spec.js
@@ -3,7 +3,16 @@ import { Ship } from "./ship";
 test("Ship takes damage", () => {
   const ship = new Ship(5);
   ship.hit();
-  const actual = ship.getHits();
-  const expected = 4;
+  const actual = ship.hits;
+  const expected = 1;
+  expect(actual).toBe(expected);
+});
+
+test("Ship sinks", () => {
+  const ship = new Ship(2);
+  ship.hit();
+  ship.hit();
+  const actual = ship.isSunk();
+  const expected = true;
   expect(actual).toBe(expected);
 });
