79779a25af2fb905ab87bb380f15b804d9a025de
David Zhu <davidyzhu2@gmail.com>
2025-01-06 11:31:45 -0800

    Add gameboard class and tests

---
+++ gameboard.js
@@ -0,0 +1,64 @@
+export class Gameboard {
+  board;
+  misses;
+  shipList;
+
+  constructor() {
+    this.misses = [];
+    this.board = [];
+    this.shipList = [];
+    for (let i = 0; i < 10; i++) {
+      this.board.push([]);
+      for (let j = 0; j < 10; j++) {
+        this.board[i].push(0);
+      }
+    }
+  }
+
+  canPlace(x, y, ship, vertical = false) {
+    if (x < 0 || x >= 10 || y < 0 || y >= 10) return false;
+    if (!vertical) {
+      if (x + ship.length >= 10) return false;
+
+      for (let i = x; i < x + ship.length; i++) {
+        if (this.board[i][y] != 0) return false;
+      }
+    } else {
+      if (y + ship.length >= 10) return false;
+
+      for (let j = y; j < y + ship.length; j++) {
+        if (this.board[x][j] != 0) return false;
+      }
+    }
+    return true;
+  }
+
+  placeShip(x, y, ship, vertical = false) {
+    if (!this.canPlace(x, y, ship, vertical)) return;
+    if (!vertical) {
+      for (let i = x; i < x + ship.length; i++) {
+        this.board[i][y] = ship;
+      }
+    } else {
+      for (let j = y; j < y + ship.length; j++) {
+        this.board[x][j] = ship;
+      }
+    }
+    this.shipList.push(ship);
+  }
+
+  receiveAttack(x, y) {
+    if (this.board[x][y] == 0) {
+      this.misses.push([x, y]);
+    } else {
+      this.board[x][y].hit();
+    }
+  }
+
+  sunkAll() {
+    this.shipList.forEach((element) => {
+      if (!element.isSunk()) return false;
+    });
+    return true;
+  }
+}
---
+++ gameboard.spec.js
@@ -0,0 +1,65 @@
+import { Gameboard } from "./gameboard";
+import { Ship } from "./ship";
+
+test("Cannot place out of bounds", () => {
+  const game = new Gameboard();
+  const ship = new Ship(3);
+  expect(game.canPlace(9, 9, ship)).toBe(false);
+});
+
+test("Cannot place out of bounds vertically", () => {
+  const game = new Gameboard();
+  const ship = new Ship(3);
+  expect(game.canPlace(0, 9, ship, true)).toBe(false);
+});
+
+test("Place ship", () => {
+  const game = new Gameboard();
+  const ship = new Ship(3);
+  game.placeShip(3, 4, ship);
+  for (let i = 3; i < 6; i++) {
+    expect(game.board[i][4]).toBe(ship);
+  }
+});
+
+test("Place vertical ship", () => {
+  const game = new Gameboard();
+  const ship = new Ship(3);
+  game.placeShip(3, 4, ship, true);
+  for (let i = 4; i < 7; i++) {
+    expect(game.board[3][i]).toBe(ship);
+  }
+});
+
+test("Hit ship", () => {
+  const game = new Gameboard();
+  const ship = new Ship(3);
+  game.placeShip(3, 4, ship);
+  game.receiveAttack(4, 4);
+  expect(ship.hits).toBe(1);
+});
+
+test("Miss ship", () => {
+  const game = new Gameboard();
+  const ship = new Ship(3);
+  game.placeShip(3, 4, ship);
+  game.receiveAttack(8, 4);
+  game.receiveAttack(9, 5);
+  expect(ship.hits).toBe(0);
+  expect(game.misses).toEqual([
+    [8, 4],
+    [9, 5],
+  ]);
+});
+
+test("Sunk all", () => {
+  const game = new Gameboard();
+  const ship = new Ship(2);
+  const ship2 = new Ship(1);
+  game.placeShip(2, 1, ship);
+  game.placeShip(4, 3, ship2);
+  game.receiveAttack(2, 1);
+  game.receiveAttack(3, 1);
+  game.receiveAttack(4, 3);
+  expect(game.sunkAll()).toBe(true);
+});
